2e406ecd80fe2899ad665d719cbec09f68f0dbb3
Deepe <ossworupojha@gmail.com>
2025-02-20 00:01:16 +0530

    production commit

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 app.use('/user', userRoute);
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
